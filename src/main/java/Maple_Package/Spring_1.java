5d44998dbede23f7880adbacfecb83254afa11b8
RIVER PLASTICK <riveros546@hotmail.com>
2022-08-19 20:07:09 -0500

    Este es el primer commit de Nicolas

--- src/main/java/Maple_Package/Spring_1.java
+++ src/main/java/Maple_Package/Spring_1.java
@@ -4,6 +4,7 @@ public class Spring_1 {
     public static void main(String[] args) {
 
         System.out.println("Hola Mundo");
+        System.out.println("Este es el primer commit de Nicolas");
 
     }
 }
