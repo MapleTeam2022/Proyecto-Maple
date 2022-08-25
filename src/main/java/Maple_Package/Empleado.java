2c35bb6487d96096b85aa53cd744842ff7ed1553
RIVER PLASTICK <riveros546@hotmail.com>
2022-08-24 19:33:21 -0500

    Este es el commit del Sprint 2

---
+++ src/main/java/Maple_Package/Empleado.java
@@ -0,0 +1,51 @@
+package Maple_Package;
+
+public class Empleado {
+
+    private String nombre;
+    private String correo;
+    Empresa empresa;
+    private String rol;
+
+    boolean admin;
+    boolean operativo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        if (rol == "administrador"){
+            this.admin = true;
+            this.operativo = false;
+        }
+        else{
+            this.admin = false;
+            this.operativo = true;
+        }
+    }
+}
---
+++ src/main/java/Maple_Package/MovimientoDinero.java
@@ -0,0 +1,32 @@
+package Maple_Package;
+
+public class MovimientoDinero {
+
+    private int monto;
+    private String concepto;
+    Empleado empleado;
+
+    public int getMonto() {
+        return monto;
+    }
+
+    public void setMonto(int monto) {
+        this.monto = monto;
+    }
+
+    public String getConcepto() {
+        return concepto;
+    }
+
+    public void setConcepto(String concepto) {
+        this.concepto = concepto;
+    }
+
+    public Empleado getEmpleado() {
+        return empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+}
